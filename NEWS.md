d89749133c417c2a8441cbd51323261d46a84c3e
Ben Baumer <ben.baumer@gmail.com>
2017-09-29 16:49:52 -0400

    prepare for CRAN submission

---
+++ NEWS.md
@@ -0,0 +1,6 @@
+# teamcolors 0.0.1
+
+* Added a `NEWS.md` file to track changes to the package.
+
+
+
--- R/data.R
+++ R/data.R
@@ -14,7 +14,7 @@
 #' @details The colors given are HTML hexidecimal values. See \code{\link[grDevices]{colors}}
 #' for more information. 
 #' 
-#' @source \url{https://github.com/teamcolors/teamcolors.github.io/tree/master/src/scripts/data/leagues}
+#' @source \url{http://jim-nielsen.com/teamcolors/}
 #' 
 #' @examples 
 #' data(teamcolors)
@@ -27,13 +27,20 @@
 #'     # St. Louis Cardinals do not match
 #'     left_join(teamcolors, by = "name")
 #'   with(pythag, plot(exp_wpct, wpct, bg = primary, col = secondary, pch = 21, cex = 3))
-#' }
+#' 
+#' 
+#' # Using ggplot2
 #' if (require(ggplot2)) {
-#'   ggplot(data = pythag, aes(x = exp_wpct, y = wpct)) + 
-#'     geom_point(shape = 21, size = 5, fill = pythag$primary, color = pythag$secondary) + 
-#'     geom_text(aes(label = name), size = 3, nudge_y = -0.005) + 
-#'     geom_abline(slope = 1, intercept = 0, lty = 3) + 
+#'   ggplot(pythag, aes(x = wpct, y = exp_wpct, color = name, fill = name)) + 
+#'     geom_abline(slope = 1, intercept = 0, linetype = 3) + 
+#'     geom_point(shape = 21, size = 3) + 
+#'     scale_fill_manual(values = pythag$primary, guide = FALSE) + 
+#'     scale_color_manual(values = pythag$secondary, guide = FALSE) + 
+#'     geom_text(aes(label = substr(name, 1, 3))) + 
+#'     scale_x_continuous("Winning Percentage", limits = c(0.3, 0.7)) + 
+#'     scale_y_continuous("Expected Winning Percentage", limits = c(0.3, 0.7)) + 
 #'     coord_equal()
+#'   }
 #' }
 #' 
 "teamcolors"
