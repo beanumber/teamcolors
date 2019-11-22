17ec10987fbda5ca5713d8bf4836a36e53e89e76
Paige PAtrick <ppatrick@smith.edu>
2019-11-21 22:32:39 -0600

    Worked on fixing comments related to tribble and regular expressions

--- R/locations_and_mascots.R
+++ R/locations_and_mascots.R
@@ -1,25 +1,18 @@
 ## LOCATIONS AND MASCOTS (by: pep)
-## for: MLB, NBA, NCAA, NFL, and NHL (NOT: EPL or MLS)
+## FOR: MLB, NBA, NCAA, NFL, and NHL (NOT: EPL or MLS)
 
 ## please run the ncaa_color_update script
 ## please run the logos.R script
 ## make sure to load rvest after tidyverse (purr)
+
 library(tidyverse)
-library(readr)
 library(rvest) 
 
 
 ## Removing epl and mls leagues
 
 teamcolors_without_epl_mls <- teamcolors %>%
-  filter(league == "mlb" | league == "nba" |
-           league == "nfl" | league == "nhl" |
-           league == "ncaa")
-
-## removing the weird X1 from the datasets
-teamcolors_without_epl_mls <- teamcolors_without_epl_mls %>%
-  select(-X1)
-
+  filter(league %in% c("mlb","nba","nfl","nhl","ncaa"))
 
 ## this is a function that is called on in the name_location_mascot_function below
 ## the only thing that this function does is replaces all of the specificed strings to include an underscore
@@ -27,146 +20,75 @@ teamcolors_without_epl_mls <- teamcolors_without_epl_mls %>%
 
 nasty_gsub_function <- function(df){
   
-  df <- mutate(df,    ## mlb, nfl, nba, nhl
+  df <- mutate(df,
                
-               name = gsub("Golden State", "Golden_State", name),
-               name = gsub("Green Bay", "Green_Bay", name),
-               name = gsub("Kansas City", "Kansas_City", name),
-               name = gsub("Los Angeles", "Los_Angeles", name),
-               name = gsub("New England", "New_England", name),
-               name = gsub("New Jersey", "New_Jersey", name),
-               name = gsub("New Orleans", "New_Orleans", name),
-               name = gsub("New York", "New_York", name),
-               name = gsub("Oklahoma City", "Oklahoma_City", name),
-               name = gsub("San Antonio", "San_Antonio", name),
-               name = gsub("San Diego", "San_Diego", name),
-               name = gsub("San Francisco", "San_Francisco", name),
-               name = gsub("San Jose", "San_Jose", name),
-               name = gsub("St. Louis", "St._Louis", name),
-               name = gsub("Tampa Bay", "Tampa_Bay", name),
+               ## Taking care of the spaces after the first word of a name that has at least two words
+               ## Don't use any state news, or it will replace underscores for the spaces of all colleges in that state
                
-               ## ncaa (nasty part)
+               name = gsub("^East ", "East_", name),
+               name = gsub("^Eastern ", "Eastern_", name),
+               name = gsub("^George ", "George_", name),
+               name = gsub("^Long ", "Long_", name),
+               name = gsub("^Los ", "Los_", name),
+               name = gsub("^New ", "New_", name),
+               name = gsub("^North ", "North_", name),
+               name = gsub("^Northern ", "Northern_", name),
+               name = gsub("^San ", "San_", name),
+               name = gsub("^St. ", "St._", name),
+               name = gsub("^Saint ", "Saint_", name),
+               name = gsub("^South ", "South_", name),
+               name = gsub("^Southern ", "Southern_", name),
+               name = gsub("^West ", "West_", name),
+               name = gsub("^Western ", "Western_", name),
                
-               name = gsub("Air Force", "Air_Force", name), 
-               name = gsub("Appalachian State", "Appalachian_State", name),
-               name = gsub("Arkansas State", "Arkansas_State", name),
-               name = gsub("Arizona State", "Arizona_State", name),
-               name = gsub("Ball State", "Ball_State", name),
-               name = gsub("Boise State", "Boise_State", name),
-               name = gsub("Boston College", "Boston_College", name),
+               # Taking Care of the spaces before the second word of a name that has at least two words
+               
+               name = gsub(" Bay", "_Bay", name),
+               name = gsub(" City", "_City", name),
+               name = gsub(" College", "_College", name),
+               name = gsub(" Fullerton", "_Fullerton", name),
+               name = gsub(" Northridge", "_Northridge", name),
+               name = gsub(" State", "_State", name),
+               name = gsub(" Tech", "_Tech", name),
+               
+               # The rest that could'nt be covered with a group of other expressions
+               
+               name = gsub("Air Force", "Air_Force", name),
                name = gsub("Bowling Green", "Bowling_Green", name), 
                name = gsub("Brigham Young", "Brigham_Young", name),
                name = gsub("Cal Poly", "Cal_Poly", name),
-               name = gsub("Cal State Fullerton", "Cal_State_Fullerton", name),
+               name = gsub("California Davis", "California_Davis", name),
+               name = gsub("California Riverside", "California_Riverside", name),
                name = gsub("Central Michigan", "Central_Michigan", name),
-               name = gsub("Chicago State", "Chicago_State", name),
-               name = gsub("Cleveland State", "Cleveland_State", name), 
                name = gsub("Coastal Carolina", "Coastal_Carolina", name),
-               name = gsub("Colorado State", "Colorado_State", name),
                name = gsub("CSU Bakersfield", "CSU_Bakersfield", name),
-               name = gsub("Cal State Northridge", "Cal_State_Northridge", name),
-               name = gsub("East Carolina", "East_Carolina", name),
-               name = gsub("Eastern Michigan", "Eastern_Michigan", name), 
-               name = gsub("Eastern Washington", "Eastern_Washington", name),
-               name = gsub("Florida Gulf Coast", "Florida_Gulf_Coast", name),
                name = gsub("Florida Atlantic", "Florida_Atlantic", name),
-               name = gsub("Florida State", "Florida_State", name),
-               name = gsub("Fresno State", "Fresno_State", name),
-               name = gsub("George Mason", "George_Mason", name),
-               name = gsub("George Washington", "George_Washington", name), 
+               name = gsub("Florida Gulf Coast", "Florida_Gulf_Coast", name),
                name = gsub("Georgia Southern", "Georgia_Southern", name),
-               name = gsub("Georgia State", "Georgia_State", name),
-               name = gsub("Georgia Tech", "Georgia_Tech", name),
                name = gsub("Grand Canyon", "Grand_Canyon", name),
-               name = gsub("Wisconsin-Green Bay", "Wisconsin-Green_Bay", name),
-               name = gsub("Idaho State", "Idaho_State", name), 
-               name = gsub("Illinois State", "Illinois_State", name),
-               name = gsub("Indiana State", "Indiana_State", name),
-               name = gsub("Iowa State", "Iowa_State", name),
-               name = gsub("Jacksonville State", "Jacksonville_State", name),
-               name = gsub("Kansas State", "Kansas_State", name),
-               name = gsub("Kennesaw State", "Kennesaw_State", name), 
-               name = gsub("Kent State", "Kent_State", name),
                name = gsub("La Salle", "La_Salle", name),
                name = gsub("Little Rock", "Little_Rock", name),
                name = gsub("Loyola Marymount", "Loyola_Marymount", name),
-               name = gsub("Long Beach State", "Long_Beach_State", name),
-               name = gsub("Louisiana Tech", "Louisiana_Tech", name),
                name = gsub("Miami (Ohio)", "Miami_(Ohio)", name), 
-               name = gsub("Michigan State", "Michigan_State", name),
                name = gsub("Middle Tennessee", "Middle_Tennessee", name),
-               name = gsub("Mississippi State", "Mississippi_State", name),
-               name = gsub("Missouri State", "Missouri_State", name),
-               name = gsub("Montana State", "Montana_State", name),
-               name = gsub("North Carolina State", "North_Carolina_State", name),
-               name = gsub("New Hampshire", "New_Hampshire", name),
-               name = gsub("New Mexico State", "New_Mexico_State", name),
-               name = gsub("New Mexico", "New_Mexico", name),
-               name = gsub("North Alabama", "North_Alabama", name),
-               name = gsub("North Carolina", "North_Carolina", name),
-               name = gsub("North Dakota State", "North_Dakota_State", name),
-               name = gsub("North Texas", "North_Texas", name), 
-               name = gsub("Northern Arizona", "Northern_Arizona", name),
-               name = gsub("Northern Colorado", "Northern_Colorado", name),
-               name = gsub("Northern Illinois", "Northern_Illinois", name),
-               name = gsub("Northern Kentucky", "Northern_Kentucky", name),
                name = gsub("Notre Dame", "Notre_Dame", name),
-               name = gsub("Ohio State", "Ohio_State", name), 
-               name = gsub("Oklahoma State", "Oklahoma_State", name),
                name = gsub("Old Dominion", "Old_Dominion", name),
                name = gsub("Oral Roberts", "Oral_Roberts", name),
-               name = gsub("Oregon State", "Oregon_State", name),
-               name = gsub("Penn State", "Penn_State", name),
-               name = gsub("Portland State", "Portland_State", name), 
                name = gsub("Purdue Fort Wayne", "Purdue_Fort_Wayne", name),
-               name = gsub("Rhode Island", "Rhode_Island", name),
-               name = gsub("Sacramento State", "Sacramento_State", name),
-               name = gsub("St. Joseph's", "St._Joseph's", name),
-               name = gsub("Saint Louis", "Saint_Louis", name),
-               name = gsub("Saint Marys", "Saint_Marys", name), 
-               name = gsub("Saint Peter's", "Saint_Peter's", name),
-               name = gsub("St. Bonaventure", "St._Bonaventure", name),
-               name = gsub("St. John's", "St._John's", name),
-               name = gsub("San Diego State", "San_Diego_State", name),
-               name = gsub("San Diego", "San_Diego", name),
-               name = gsub("San Francisco", "San_Francisco", name),
-               name = gsub("San Jose State", "San_Jose_State", name), 
                name = gsub("Santa Clara", "Santa_Clara", name),
                name = gsub("Seton Hall", "Seton_Hall", name),
-               name = gsub("South Alabama", "South_Alabama", name),
-               name = gsub("South Carolina", "South_Carolina", name),
-               name = gsub("South Dakota State", "South_Dakota_State", name),
-               name = gsub("South Dakota", "South_Dakota", name),
-               name = gsub("South Florida", "South_Florida", name), 
-               name = gsub("Southern California", "Southern_California", name),
-               name = gsub("Southern Illinois", "Southern_Illinois", name),
-               name = gsub("Southern Miss", "Southern_Miss", name),
-               name = gsub("Southern Utah", "Southern_Utah", name),
                name = gsub("Stony Brook", "Stony_Brook", name),
                name = gsub("Texas A&M", "Texas_A&M", name), 
-               name = gsub("Texas State", "Texas_State", name),
-               name = gsub("Texas Tech", "Texas_Tech", name),
-               name = gsub("California Davis", "California_Davis", name),
-               name = gsub("California Riverside", "California_Riverside", name),
                name = gsub("UMass Lowell", "UMass_Lowell", name),
-               name = gsub("Northern Iowa", "Northern_Iowa", name), 
                name = gsub("USC Upstate", "USC_Upstate", name),
-               name = gsub("Utah State", "Utah_State", name),
                name = gsub("Utah Valley", "Utah_Valley", name),
                name = gsub("Virginia Commonwealth", "Virginia_Commonwealth", name),
-               name = gsub("Virginia Tech", "Virginia_Tech", name),
-               name = gsub("Wake Forest", "Wake_Forest", name), 
-               name = gsub("Washington State", "Washington_State", name),
-               name = gsub("Weber State", "Weber_State", name),
-               name = gsub("West Virginia", "West_Virginia", name),
-               name = gsub("Western Illinois", "Western_Illinois", name),
-               name = gsub("Western Kentucky", "Western_Kentucky", name),
-               name = gsub("Western Michigan", "Western_Michigan", name), 
-               name = gsub("Wichita State", "Wichita_State", name),
-               name = gsub("Wright State", "Wright_State", name),
-               name = gsub("Youngstown State", "Youngstown_State", name))
+               name = gsub("Wake Forest", "Wake_Forest", name))
+  
+  new_gsub_test <<- data.frame(df)
   
   return(df)
+  
 }
 
 
@@ -228,6 +150,3 @@ teamcolors_updated <- teamcolors %>%
 save(teamcolors_updated, file = "data/teamcolors_updated.rda", compress = "xz")
 
 ## attempt to run R CMD check
-
-
-
--- data-raw/logos.R
+++ data-raw/logos.R
@@ -48,97 +48,100 @@ logos <- leagues %>%
 
 
 ## Renaming ncaa team color names, so that they match up with the join
-## This is not the best way to do this, but it was important to keep the mascots in the team names
+## Created a tribble, in order to join the names later with the existing teamcolors dataset
 
-## Old name from teamcolors dataset                                      <- New name to match logos dataset
-
-teamcolors$name[teamcolors$name == "Air Force"]                          <- "Air Force Falcons"
-teamcolors$name[teamcolors$name == "Albany (NY)"]                        <- "Albany Great Danes"
-teamcolors$name[teamcolors$name == "Bowling Green"]                      <- "Bowling Green Falcons"
-teamcolors$name[teamcolors$name == "Buffalo"]                            <- "Buffalo Bulls"
-teamcolors$name[teamcolors$name == "BYU"]                                <- "Brigham Young Cougars"
-teamcolors$name[teamcolors$name == "Cal Poly"]                           <- "Cal Poly Mustangs"
-teamcolors$name[teamcolors$name == "Cal State Fullerton"]                <- "Cal State Fullerton Titans"
-teamcolors$name[teamcolors$name == "California"]                         <- "California Golden Bears"
-teamcolors$name[teamcolors$name == "Canisius"]                           <- "Canisius Golden Griffins"
-teamcolors$name[teamcolors$name == "Charlotte"]                          <- "Charlotte 49ers"
-teamcolors$name[teamcolors$name == "Colorado"]                           <- "Colorado Buffaloes"
-teamcolors$name[teamcolors$name == "CSU Bakersfield"]                    <- "CSU Bakersfield Roadrunners"
-teamcolors$name[teamcolors$name == "CSUN"]                               <- "Cal State Northridge Matadors"
-teamcolors$name[teamcolors$name == "Dartmouth"]                          <- "Dartmouth Big Green"
-teamcolors$name[teamcolors$name == "Davidson"]                           <- "Davidson Wildcats"
-teamcolors$name[teamcolors$name == "Detroit Mercy Titans"]               <- "Detroit Titans"
-teamcolors$name[teamcolors$name == "FGCU"]                               <- "Florida Gulf Coast Eagles"
-teamcolors$name[teamcolors$name == "FIU"]                                <- "FIU Panthers"
-teamcolors$name[teamcolors$name == "Fresno State"]                       <- "Fresno State Bulldogs"
-teamcolors$name[teamcolors$name == "Georgia Tech"]                       <- "Georgia Tech Yellow Jackets"
-teamcolors$name[teamcolors$name == "Green Bay"]                          <- "Wisconsin-Green Bay Phoenix"
-teamcolors$name[teamcolors$name == "Hawaii"]                             <- "Hawaii Warriors"
-teamcolors$name[teamcolors$name == "Illinois"]                           <- "Illinois Fighting Illini"
-teamcolors$name[teamcolors$name == "Iona"]                               <- "Iona Gaels"
-teamcolors$name[teamcolors$name == "IUPUI"]                              <- "IUPUI Jaguars"
-teamcolors$name[teamcolors$name == "Little Rock"]                        <- "Little Rock Trojans"
-teamcolors$name[teamcolors$name == "LMU (CA)"]                           <- "Loyola Marymount Lions"
-teamcolors$name[teamcolors$name == "Long Beach State"]                   <- "Long Beach State 49ers"
-teamcolors$name[teamcolors$name == "Louisiana"]                          <- "Louisiana Ragin Cajuns"
-teamcolors$name[teamcolors$name == "Louisiana-Monroe"]                   <- "Louisiana-Monroe Warhawks"
-teamcolors$name[teamcolors$name == "Loyola Chicago Ramblers"]            <- "Loyola Ramblers"
-teamcolors$name[teamcolors$name == "LSU"]                                <- "LSU Tigers"
-teamcolors$name[teamcolors$name == "Manhattan"]                          <- "Manhattan Jaspers"
-teamcolors$name[teamcolors$name == "Marist"]                             <- "Marist Red Foxes"
-teamcolors$name[teamcolors$name == "Maryland"]                           <- "Maryland Terrapins"
-teamcolors$name[teamcolors$name == "Massachusetts"]                      <- "Massachusetts Minutemen"
-teamcolors$name[teamcolors$name == "Miami (FL)"]                         <- "Miami Hurricanes"
-teamcolors$name[teamcolors$name == "Miami (OH)"]                         <- "Miami (Ohio) Redhawks"
-teamcolors$name[teamcolors$name == "Middle Tennessee"]                   <- "Middle Tennessee Blue Raiders"
-teamcolors$name[teamcolors$name == "Milwaukee"]                          <- "Wisconsin-Milwaukee Panthers"
-teamcolors$name[teamcolors$name == "Navy"]                               <- "Navy Midshipmen"
-teamcolors$name[teamcolors$name == "NC State"]                           <- "North Carolina State Wolfpack"
-teamcolors$name[teamcolors$name == "Nebraska"]                           <- "Nebraska Cornhuskers"
-teamcolors$name[teamcolors$name == "Nevada"]                             <- "Nevada Wolf Pack"
-teamcolors$name[teamcolors$name == "NJIT"]                               <- "NJIT Highlanders"
-teamcolors$name[teamcolors$name == "North Alabama"]                      <- "North Alabama Lions"
-teamcolors$name[teamcolors$name == "North Carolina"]                     <- "North Carolina Tar Heels"
-teamcolors$name[teamcolors$name == "North Florida Ospreys"]              <- "UNF Ospreys"
-teamcolors$name[teamcolors$name == "Ohio State"]                         <- "Ohio State Buckeyes"
-teamcolors$name[teamcolors$name == "Oklahoma State"]                     <- "Oklahoma State Cowboys"
-teamcolors$name[teamcolors$name == "Ole Miss"]                           <- "Mississippi Rebels"
-teamcolors$name[teamcolors$name == "Omaha"]                              <- "Nebraska-Omaha Mavericks"
-teamcolors$name[teamcolors$name == "Pacific"]                            <- "Pacific Tigers"
-teamcolors$name[teamcolors$name == "Penn"]                               <- "Penn Quakers"
-teamcolors$name[teamcolors$name == "Penn State"]                         <- "Penn State Nittany Lions"
-teamcolors$name[teamcolors$name == "Providence"]                         <- "Providence Friars"
-teamcolors$name[teamcolors$name == "Sacramento State"]                   <- "Sacramento State Hornets"
-teamcolors$name[teamcolors$name == "Saint Joseph's Hawks"]               <- "St. Joseph's Hawks"
-teamcolors$name[teamcolors$name == "Saint Mary's (CA)"]                  <- "Saint Marys Gaels"
-teamcolors$name[teamcolors$name == "SaintBonaventure"]                   <- "St. Bonaventure Bonnies"
-teamcolors$name[teamcolors$name == "SaintJohn's (NY)"]                   <- "St. John's Red Storm"
-teamcolors$name[teamcolors$name == "Seattle U"]                          <- "Seattle Redhawks"
-teamcolors$name[teamcolors$name == "Siena"]                              <- "Siena Saints"
-teamcolors$name[teamcolors$name == "Southern California Trojans[A 31]"]  <- "Southern California Trojans"
-teamcolors$name[teamcolors$name == "Southern Illinois"]                  <- "Southern Illinois Salukis"
-teamcolors$name[teamcolors$name == "Southern Mississippi Golden Eagles"] <- "Southern Miss Golden Eagles"
-teamcolors$name[teamcolors$name == "TCU"]                                <- "TCU Horned Frogs"
-teamcolors$name[teamcolors$name == "Texas"]                              <- "Texas Longhorns"
-teamcolors$name[teamcolors$name == "UAB"]                                <- "UAB Blazers"
-teamcolors$name[teamcolors$name == "UC Davis"]                           <- "California Davis Aggies"
-teamcolors$name[teamcolors$name == "UC Irvine"]                          <- "California-Irvine Anteaters"
-teamcolors$name[teamcolors$name == "UC Riverside"]                       <- "California Riverside Highlanders"
-teamcolors$name[teamcolors$name == "UCLA"]                               <- "UCLA Bruins"
-teamcolors$name[teamcolors$name == "UIC"]                                <- "Illinois-Chicago Flames"
-teamcolors$name[teamcolors$name == "UMass Lowell"]                       <- "UMass Lowell River Hawks"
-teamcolors$name[teamcolors$name == "UMBC"]                               <- "UMBC Retrievers"
-teamcolors$name[teamcolors$name == "UMKC"]                               <- "UMKC Kangaroos"
-teamcolors$name[teamcolors$name == "UNI"]                                <- "Northern Iowa Panthers"
-teamcolors$name[teamcolors$name == "UNLV"]                               <- "UNLV Rebels"
-teamcolors$name[teamcolors$name == "USC Upstate"]                        <- "USC Upstate Spartans"
-teamcolors$name[teamcolors$name == "UT Arlington"]                       <- "Texas-Arlington Mavericks"
-teamcolors$name[teamcolors$name == "UTEP"]                               <- "UTEP Miners"
-teamcolors$name[teamcolors$name == "UTRGV"]                              <- "UTRGV Vaqueros"
-teamcolors$name[teamcolors$name == "UTSA"]                               <- "Texas-SA Roadrunners"
-teamcolors$name[teamcolors$name == "VCU"]                                <- "Virginia Commonwealth Rams"
-teamcolors$name[teamcolors$name == "Wisconsin"]                          <- "Wisconsin Badgers"
-
+unmatched_team_names <- tibble::tribble(
+  
+  ~name, ~sportslogos_name,
+  
+   "Air Force", "Air Force Falcons", 
+   "Albany (NY)", "Albany Great Danes",
+   "Bowling Green", "Bowling Green Falcons",
+   "Buffalo", "Buffalo Bulls",
+   "BYU", "Brigham Young Cougars",
+   "Cal Poly", "Cal Poly Mustangs",
+   "Cal State Fullerton", "Cal State Fullerton Titans",
+   "California", "California Golden Bears",
+   "Canisius", "Canisius Golden Griffins",
+   "Charlotte", "Charlotte 49ers",
+   "Colorado", "Colorado Buffaloes",
+   "CSU Bakersfield", "CSU Bakersfield Roadrunners",
+   "CSUN", "Cal State Northridge Matadors",
+   "Dartmouth", "Dartmouth Big Green",
+   "Davidson", "Davidson Wildcats",
+   "Detroit Mercy Titans", "Detroit Titans",
+   "FGCU", "Florida Gulf Coast Eagles",
+   "FIU", "FIU Panthers",
+   "Fresno State", "Fresno State Bulldogs",
+   "Georgia Tech", "Georgia Tech Yellow Jackets",
+   "Green Bay", "Wisconsin-Green Bay Phoenix",
+   "Hawaii", "Hawaii Warriors",
+   "Illinois", "Illinois Fighting Illini",
+   "Iona", "Iona Gaels",
+   "IUPUI", "IUPUI Jaguars",
+   "Little Rock", "Little Rock Trojans",
+   "LMU (CA)", "Loyola Marymount Lions",
+   "Long Beach State", "Long Beach State 49ers",
+   "Louisiana", "Louisiana Ragin Cajuns",
+   "Louisiana-Monroe", "Louisiana-Monroe Warhawks",
+   "Loyola Chicago Ramblers", "Loyola Ramblers",
+   "LSU", "LSU Tigers",
+   "Manhattan", "Manhattan Jaspers",
+   "Marist", "Marist Red Foxes",
+   "Maryland", "Maryland Terrapins",
+   "Massachusetts", "Massachusetts Minutemen",
+   "Miami (FL)", "Miami Hurricanes",
+   "Miami (OH)", "Miami (Ohio) Redhawks",
+   "Middle Tennessee", "Middle Tennessee Blue Raiders",
+   "Milwaukee", "Wisconsin-Milwaukee Panthers",
+   "Navy", "Navy Midshipmen",
+   "NC State", "North Carolina State Wolfpack",
+   "Nebraska", "Nebraska Cornhuskers",
+   "Nevada", "Nevada Wolf Pack",
+   "NJIT", "NJIT Highlanders",
+   "North Alabama", "North Alabama Lions",
+   "North Carolina","North Carolina Tar Heels",
+   "North Florida Ospreys", "UNF Ospreys",
+   "Ohio State", "Ohio State Buckeyes",
+   "Oklahoma State", "Oklahoma State Cowboys",
+   "Ole Miss", "Mississippi Rebels",
+   "Omaha", "Nebraska-Omaha Mavericks",
+   "Pacific", "Pacific Tigers",
+   "Penn", "Penn Quakers",
+   "Penn State", "Penn State Nittany Lions",
+   "Providence", "Providence Friars",
+   "Sacramento State", "Sacramento State Hornets",
+   "Saint Joseph's Hawks", "St. Joseph's Hawks",
+   "Saint Mary's (CA)", "Saint Marys Gaels",
+   "SaintBonaventure", "St. Bonaventure Bonnies",
+   "SaintJohn's (NY)", "St. John's Red Storm",
+   "Seattle U", "Seattle Redhawks",
+   "Siena", "Siena Saints",
+   "Southern California Trojans[A 31]", "Southern California Trojans",
+   "Southern Illinois", "Southern Illinois Salukis",
+   "Southern Mississippi Golden Eagles", "Southern Miss Golden Eagles",
+   "TCU", "TCU Horned Frogs",
+   "Texas", "Texas Longhorns",
+   "UAB", "UAB Blazers",
+   "UC Davis", "California Davis Aggies",
+   "UC Irvine", "California-Irvine Anteaters",
+   "UC Riverside", "California Riverside Highlanders",
+   "UCLA", "UCLA Bruins",
+   "UIC", "Illinois-Chicago Flames",
+   "UMass Lowell", "UMass Lowell River Hawks",
+   "UMBC", "UMBC Retrievers",
+   "UMKC", "UMKC Kangaroos",
+   "UNI", "Northern Iowa Panthers",
+   "UNLV", "UNLV Rebels",
+   "USC Upstate", "USC Upstate Spartans",
+   "UT Arlington", "Texas-Arlington Mavericks",
+   "UTEP", "UTEP Miners",
+   "UTRGV", "UTRGV Vaqueros",
+   "UTSA", "Texas-SA Roadrunners",
+   "VCU", "Virginia Commonwealth Rams",
+   "Wisconsin", "Wisconsin Badgers"
+   
+)
 
 
 # add logos
@@ -146,10 +149,20 @@ teamcolors <- teamcolors %>%
   select(-logo) %>%
   left_join(select(logos, name = team, logo = img), by = "name")
 
-
 teamcolors %>%
   group_by(league) %>%
   summarize(num_teams = n(), num_logos = sum(!is.na(logo)))
+
+# joining teamcolors with the unmatched team names dataset, in order to get the names to match up
+
+teamcolors_test <- teamcolors %>%
+  left_join(unmatched_team_names, by = "name")
+
+## BELOW IS WHAT NEEDS TO BE FIXED
+teamcolors_test2 <- teamcolors_test %>%
+  mutate(name = ifelse(name == sportslogos_name, replace(name, sportslogos_name, sportslogos_name), name))
+
+# saving teamcolors to the data file
 
 save(teamcolors, file = "data/teamcolors.rda", compress = "xz")
 
